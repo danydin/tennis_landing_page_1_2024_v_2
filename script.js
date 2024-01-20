52fba5d84934c3317e22cd026d5d92d94a035caf
dean ben <19169228+danydin@users.noreply.github.com>
2024-01-20 19:52:51 +0800

    updated form with logic

--- script.js
+++ script.js
@@ -103,32 +103,57 @@ var colorButton = document.getElementById("changeColorBtn");
       });
     });
 
-      
-// to review:
+let isShowError = false;
+const input_1 = document.getElementById("input_1");
+const input_2 = document.getElementById("input_2");
+const input_3 = document.getElementById("input_3");
+const selectedValue = input_3.getAttribute("data-value");
+const tooltip_1 = document.getElementById("tooltip_1");
+const tooltip_2 = document.getElementById("tooltip_2");
+const tooltip_3 = document.getElementById("tooltip_3");
 
-// sending api request 
-function submitForm() {
-        // Get form data
-        var formData = new FormData(document.getElementById('myForm'));
+const numberValidataion = () => {};
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const selectInput = document.getElementById("input_3");
+  tooltip_1.style.visibility =
+    input_1.value.trim() === "" ? "visible" : "hidden";
+  tooltip_2.style.visibility = input_2.value.match(/^05\d{8}$/)
+    ? "hidden"
+    : "visible";
+  tooltip_3.style.visibility =
+    selectInput.getAttribute("data-value") == "" ? "visible" : "hidden";
+  isShowError = true;
+};
+const handleChange = () => {
+  tooltip_2.style.visibility =
+    input_2.value.trim() === "" ? "visible" : "hidden";
+  if (!input_2.value.match(/^05\d{8}$/) && input_2.value != "") {
+    tooltip_2.innerText = "מספר הטלפון אינו תקין";
+  }
 
-        // Send POST request using fetch
-        fetch('ajax-contact.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            // Handle success response
-            console.log(data);
-            // You can perform additional actions here if needed
-        })
-        .catch(error => {
-            // Handle errors
-            console.error('There was a problem with the fetch operation:', error);
-        });
-    }
+  if (isShowError) {
+    tooltip_1.style.visibility =
+      input_1.value.trim() === "" ? "visible" : "hidden";
+    tooltip_2.style.visibility = input_2.value.match(/^05\d{8}$/)
+      ? "hidden"
+      : "visible";
+  }
+};
+
+const toggleDropdown = () => {
+  var dropdown = document.getElementById("scrollbar-inner");
+  var btn = document.getElementById("btn");
+  dropdown.style.visibility =
+    dropdown.style.visibility === "visible" ? "hidden" : "visible";
+  btn.style.zIndex = dropdown.style.visibility === "hidden" ? "100" : "0";
+};
+
+function selectOption(option) {
+  const dataValue = option.getAttribute("data-value");
+  input_3.setAttribute("data-value", dataValue);
+  tooltip_3.style.visibility = selectedValue != "" ? "visible" : "hidden";
+  input_3.innerText = option.innerText;
+  toggleDropdown();
+}
+
